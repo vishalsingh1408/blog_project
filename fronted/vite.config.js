257125944d923b74fbecd39d068aeb6d7ef2cfec
vishalcodersinghji <vs8005823904@gmail.com>
2025-02-01 00:30:57 +0530

    test

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -6,10 +6,13 @@ config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {  host: '0.0.0.0',port: 5173 },
+  plugins: [react(), tailwindcss()],
+  server: {  
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['vishal-coder.onrender.com'], // Add the allowed host
+  },
   define: {
     'process.env': process.env
   }
 })
-
